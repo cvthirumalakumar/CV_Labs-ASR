24f4264955dcaad4bfe4c74e9b719c2c47e970d8
Kumar CVT <kumatcvt55@gmail.com>
2024-11-06 12:12:45 +0530

    Modified main.py

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,6 @@ from fastapi import FastAPI, UploadFile, File
 from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
 import torch
 import torchaudio
-from io import BytesIO
 from fastapi.middleware.cors import CORSMiddleware
 from datetime import datetime
 import os
@@ -33,11 +32,10 @@ async def upload_audio(audio: UploadFile = File(...)):
         with open(f"{filename}", "wb") as file_object:
             file_object.write(await audio.read())
 
-        # Run ffmpeg command to convert opus to wav format
+        # Run ffmpeg command to convert to wav format
         ffmpeg_command = f"ffmpeg -i {filename} {filename[:-5]}.wav"
         os.system(ffmpeg_command)
         
-
         waveform, sample_rate = torchaudio.load(f"{filename[:-5]}.wav")
         if sample_rate != 16000:
             resampler = torchaudio.transforms.Resample(orig_freq=sample_rate, new_freq=16000)
@@ -48,8 +46,6 @@ async def upload_audio(audio: UploadFile = File(...)):
         # delete audios 
         # os.remove(f"{filename[:-5]}.wav")
         # os.remove(f"{filename}")
-        
-        
         
         # Perform inference
         with torch.no_grad():
